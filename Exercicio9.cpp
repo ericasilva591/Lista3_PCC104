3d20f07082a6adb36d7866d5faa18d6200e19504
Erica Silva Pinto <81305764+ericasilva591@users.noreply.github.com>
2021-04-29 09:21:58 -0300

    Update Exercicio9.cpp

--- Exercicio9.cpp
+++ Exercicio9.cpp
@@ -1,56 +1,36 @@
 // Exercicio9.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
 //
 
-#include <iostream>
-#include <vector>
-
-class Arvore_Binaria {
-private:
-	struct node{
-		int v;
-		node* esquerda;
-		node* direita;
-	};
-	int n_node;
-	node* root;
-public:
-	Arvore_Binaria(int n_node);
-	void add_node(int node);
-	void busca_node(int node);
-	void apaga_node(int node);
-};
-
-Arvore_Binaria::Arvore_Binaria(int v) {
-	n_node = v;
-}
-
-void Arvore_Binaria::add_node(int node) {
-
-}
-
-void Arvore_Binaria::busca_node(int node) {
-
-}
-
-void Arvore_Binaria::apaga_node(int node) {
-
-}
+#include "Exercicio9ArvoreBinaria.cpp"
 
 int main()
 {
-	Arvore_Binaria ab(7);
-	ab.add_node(1);
-	ab.add_node(2);
-	ab.add_node(3);
-	ab.add_node(4);
-	ab.add_node(5);
-	ab.add_node(6);
-	ab.add_node(7);
+    BinarySearchTree<int> binary_tree;
 
-	ab.apaga_node(6);
-	ab.busca_node(3);
+    std::cout << (binary_tree.insert(9) == InsertionInfo::Inserted ? "Inserted" : "AlreadyIn") << std::endl;
+    std::cout << (binary_tree.insert(10) == InsertionInfo::Inserted ? "Inserted" : "AlreadyIn") << std::endl;
+    std::cout << (binary_tree.insert(20) == InsertionInfo::Inserted ? "Inserted" : "AlreadyIn") << std::endl;
+    std::cout << (binary_tree.insert(3) == InsertionInfo::Inserted ? "Inserted" : "AlreadyIn") << std::endl;
+    std::cout << (binary_tree.insert(5) == InsertionInfo::Inserted ? "Inserted" : "AlreadyIn") << std::endl;
+    std::cout << (binary_tree.insert(7) == InsertionInfo::Inserted ? "Inserted" : "AlreadyIn") << std::endl;
+    std::cout << (binary_tree.insert(9) == InsertionInfo::Inserted ? "Inserted" : "AlreadyIn") << std::endl;
+    std::cout << (binary_tree.insert(2) == InsertionInfo::Inserted ? "Inserted" : "AlreadyIn") << std::endl;
+    std::cout << (binary_tree.insert(11) == InsertionInfo::Inserted ? "Inserted" : "AlreadyIn") << std::endl;
+    std::cout << (binary_tree.insert(8) == InsertionInfo::Inserted ? "Inserted" : "AlreadyIn") << std::endl;
+    
+    binary_tree.print();
+    std::cout << std::endl;
+
+    std::cout << (binary_tree.search(15) == SearchInfo::Found ? "Found" : "NotFound") << std::endl;
+    std::cout << (binary_tree.search(2) == SearchInfo::Found ? "Found" : "NotFound") << std::endl;
+
+   
+    std::cout << (binary_tree.del(7) == DeleteInfo::Deleted ? "Deleted" : "NotDeleted") << std::endl;
+
+    binary_tree.print();
+    std::cout << std::endl;
+
 }
-
 // Executar programa: Ctrl + F5 ou Menu Depurar > Iniciar Sem Depuração
 // Depurar programa: F5 ou menu Depurar > Iniciar Depuração
 
